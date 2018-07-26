c3b19e2a8080f90c31d47bf64139b482133d8469
Charlie Andrews-Jubelt <candrewsjubelt@gmail.com>
2018-07-26 13:47:01 +0300

    completed pseudoclass refactor, still need to remove global keywords

--- student-friendly-version.py
+++ student-friendly-version.py
@@ -22,10 +22,31 @@ TIME_REMAINING_IDX = 1
 PLOT_TYPE_IDX = 0
 EMPTY_PLOT = ("EMPTY", 0)
 
+def get_new_plot(type):
+    type = type.upper()
+    time = get_grow_time(type)
+    return type, time
+
+# helper for get_new_plot
+def get_grow_time(type):
+    if type == "TOMATO":
+        return 3
+    elif type == "EGGPLANT":
+        return 150
+    elif type == "CORN":
+        return 5
+    elif type == "EMPTY":
+        return 0
+    else:
+        raise Exception("cannot get new plot of type {}".format(type))
 
 # get the image name for a plot
 def get_image(plot_tuple):
-    return plot_tuple[PLOT_TYPE_IDX] + ".gif"
+    type = plot_tuple[PLOT_TYPE_IDX]
+    if type == "EMPTY":
+        return None
+    else:
+        return type + ".gif"
 
 # get the buying price for a plot
 def get_buy_price(plot_tuple):
@@ -38,6 +59,9 @@ def get_buy_price(plot_tuple):
         return 250
     else:
         raise Exception("Cannot get price for plot of type " + plot_type)
+
+def get_time_remaining(plot_tuple):
+    return plot_tuple[TIME_REMAINING_IDX]
 
 def get_sell_price(plot_tuple):
     pass # TODO implement
@@ -53,12 +77,164 @@ START_PHASE = "setup"
 INITIAL_BALANCE = 500
 NUM_ROWS = 6
 NUM_COLS = 8
+COORD_TUPLE_X_IDX = 0
+COORD_TUPLE_Y_IDX = 1
 
 # instance properties of a farm
 current_plot_xy = (0, 0)
 phase = START_PHASE
 balance = INITIAL_BALANCE
-turtle = turtle.Turtle()
+farm_turtle = turtle.Turtle()
 state = [[EMPTY_PLOT]*NUM_COLS for row in range(NUM_ROWS)] # obvi students dont know comprehensions... but ya kno
+round = 0
 
+def draw_square(plot_tuple, plot_coords):
+    farm_turtle.pen({"speed": 0, "pencolor": "white", "pensize": 2, "pendown": False, "shown": False})
+    x = plot_coords[COORD_TUPLE_X_IDX]
+    y = plot_coords[COORD_TUPLE_Y_IDX]
+    side = PLOT_SIZE
+    if plot_coords==current_plot_xy:
+        farm_turtle.pen({"pencolor": "blue", "pensize": 3})
 
+    # Draw outline of square
+    farm_turtle.goto(x, y)
+    farm_turtle.pendown()
+    farm_turtle.setx(x + side)
+    farm_turtle.sety(y + side)
+    farm_turtle.setx(x)
+    farm_turtle.sety(y)
+
+    # Add textures!
+    image_filename = get_image(plot_tuple)
+    if image_filename != None:
+        farm_turtle.goto(x+side/2, y+side/2)
+        time_remaining = get_time_remaining(plot_tuple)
+        if time_remaining == 1:
+            farm_turtle.shape("money.gif")
+        else:
+            farm_turtle.shape(image_filename)
+        farm_turtle.stamp()
+
+def write_balance():
+    x = PLOT_SIZE * NUM_COLS / 2
+    y = PLOT_SIZE * NUM_ROWS / 2
+    farm_turtle.penup()
+    farm_turtle.goto(x,y)
+    farm_turtle.pendown()
+    farm_turtle.write(str(balance), font=("Arial", 12, "normal"))
+    
+def write_round():
+    x = -PLOT_SIZE*NUM_COLS/2
+    y = PLOT_SIZE*NUM_ROWS/2
+    farm_turtle.penup()
+    farm_turtle.goto(x,y)
+    farm_turtle.pendown()
+    farm_turtle.write("Round: " + str(round+1), font=("Arial", 12, "normal"))
+    
+def write_score():
+    farm_turtle.clear()
+    farm_turtle.penup()
+    farm_turtle.goto(0, 0)
+    farm_turtle.pendown()
+    farm_turtle.write("SCORE: " + str(balance), font=("Arial", 48, "normal"), align="center")
+    turtle.update()
+    
+def render():
+    """"Renders all plots on the screen."""
+    farm_turtle.clear()
+    farm_turtle.hideturtle()
+    farm_turtle.tracer(0, 0)
+    for plot in self.plots.values():
+        if plot != self.current_plot:
+            self.drawsquare(farm_turtle, plot)
+    self.drawsquare(farm_turtle, self.current_plot)
+    self.write_balance()
+    self.write_round(round)
+    turtle.update()
+
+"""
+Helper functions for setup. Change the highlighted plot based on
+the keypress.
+"""
+def select_up():
+    global current_plot_xy
+    if current_plot_xy[COORD_TUPLE_Y_IDX] != Constants.Y_MAXIMUM - 1:
+        current_plot_xy = state[current_plot_xy[COORD_TUPLE_x_IDX]][current_plot_xy[COORD_TUPLE_Y_IDX]+1]
+
+def select_down():
+    global current_plot_xy
+    if current_plot_xy[COORD_TUPLE_Y_IDX] != 0:
+        current_plot_xy = state[current_plot_xy[COORD_TUPLE_x_IDX]][current_plot_xy[COORD_TUPLE_Y_IDX]-1]
+
+def select_left():
+    global current_plot_xy
+    if current_plot_xy[COORD_TUPLE_Y_IDX] != 0:
+        current_plot_xy = state[current_plot_xy[COORD_TUPLE_x_IDX]-1][current_plot_xy[COORD_TUPLE_Y_IDX]]
+
+def select_right():
+    global current_plot_xy
+    if current_plot_xy[COORD_TUPLE_Y_IDX] != Constants.X_MAXIMUM - 1:
+        current_plot_xy = state[current_plot_xy[COORD_TUPLE_x_IDX]+1][current_plot_xy[COORD_TUPLE_Y_IDX]]
+
+# buy the current highlighted plot
+def buy(plot_type):
+    global current_plot_xy
+    global balance
+    global state
+    plot = state[current_plot_xy[0], current_plot_xy[1]]
+    if balance >= get_buy_price(plot):  # NOTE: allows you to buy / plant over an existing crop
+        new_plot = get_new_plot(plot_type)
+        state[current_plot_xy[0], current_plot_xy[1]] = new_plot
+        balance -= get_buy_price(new_plot)
+
+def buy_corn():
+    buy("corn")
+
+def buy_eggplant():
+    buy("eggplant")
+
+def buy_tomato():
+    buy("tomato")
+
+def play():
+    global round
+    while round <= 9:
+        setup()
+        timestep()
+        self.round += 1
+    write_score()
+
+def setup():
+    """
+    Manual stage of each turn. The blue highlight indicates currently-selected plot.
+    Player-controlled buy/sell transactions occur here.
+    """
+    global phase
+    while phase == 'setup':
+        render()
+        turtle.onkey(select_up, "Up")
+        turtle.onkey(select_down, "Down")
+        turtle.onkey(select_left, "Left")
+        turtle.onkey(select_right, "Right")
+        turtle.onkey(buy_corn, "c")
+        turtle.onkey(buy_eggplant, "e")
+        turtle.onkey(buy_tomato, "t")
+        turtle.onkey(start_timestep, "p")
+        turtle.listen()
+
+def start_timestep():
+    global phase
+    phase = "timestep"
+
+def timestep():
+    global state
+    for row_idx in range(NUM_ROWS):
+        for col_idx in range(NUM_COLS):
+            plot = state[row_idx][col_idx]
+            new_plot = decrement_time(plot)
+            state[row_idx][col_idx] = new_plot
+            if new_plot[TIME_REMAINING_IDX] == 0:
+                balance += get_sell_price(new_plot)
+                state[row_idx][col_idx] = EMPTY_PLOT
+    global phase
+    phase = "setup"
